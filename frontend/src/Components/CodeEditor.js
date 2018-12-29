afb31007345416ad38f1bd72dcf86fd6bd11f204
Jaro Habiger <jarohabiger@googlemail.com>
2018-12-29 20:07:26 +0100

    add no gallery component

--- frontend/src/Components/CodeEditor.js
+++ frontend/src/Components/CodeEditor.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import MonacoEditor from 'react-monaco-editor';
+import { IoMdApps, IoMdPlay } from 'react-icons/io';
 
 export function CodeEditor({ code, onChange, onRun }) {
   return (
     <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-      <div style={{ flex: '0 0 50px' }}>
-        <button type="button" onClick={onRun}>
-          Run
-        </button>
+      <div style={{ 
+        flex: '0 0 50px', 
+        display: 'flex',
+        alignItems: 'center',
+        color: '#fff',
+        justifyContent: 'space-between',  
+        margin: '0 20px',
+        fontSize: '30px',
+      }}>
+        <div>
+          <IoMdApps />
+        </div>
+        <input type='text' />
+        <div>
+          <IoMdPlay onClick={onRun}></IoMdPlay>
+        </div>
       </div>
       <div className="monaco" style={{ height: '100%', overflow: 'hidden' }}>
         <MonacoEditor
